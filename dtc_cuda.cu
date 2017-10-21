f22e6cc9a07c1658f48da5173b648f34864f365f
hafeezali <hafeez.ali5@gmail.com>
2017-10-21 17:09:49 +0530

    Added getInfoGainsOfData & getInfoGains kernels

--- dtc_cuda.cu
+++ dtc_cuda.cu
@@ -7,6 +7,7 @@
 #include "math.h"
 #include "limits.h"
 
+#define INT_MIN -9999999
 #define trainingData "myDataset.txt"
 #define testingData "myDataset.txt"
 #define M 6
@@ -21,7 +22,7 @@ vector <vector <int> > testFile;
 
 int *d_trainFileData, *d_cardinality;
 
-dim3 blocks(M);
+dim3 blocks(M-1);
 dim3 threads(M,N);
 
 // node structure of the decision tree
@@ -90,12 +91,62 @@ void readCSV(string str)
 	}
 }
 
-__global__
+__global__ void getInfoGains(int *attr,int *data,int dataSize,float *infoGains,int *trainFileData,int *cardinality)
+{
+	infoGains[blockIdx.x]=INT_MIN;
+	__syncthreads();
+	if(attr[blockIdx.x]==0){
+		int tid,bid;
+		float infoGain,intermediateGain;
+		__shared__ int attrValCount[cardinality[bid]+1];
+		__shared__ int classAttrValCount[cardinality[bid]+1][cardinality[M]+1];
+		tid=threadIdx.x;
+		bid=blockIdx.x;
+		int classVal = trainFileData(data[tid],M-1);
+		int attrVal = trainFileData(data[tid],bid);
+		atomicAdd(&attrValCount[attrVal],1);
+		atomicAdd(&classAttrValCount[attrVal][classVal],1);
+		__syncthreads();
+		infoGain=0;
+		if(tid==0){
+			int i,j;
+			for(i=1;i<=cardinality[bid];i++){
+				intermediateGain=0;
+				for(j=1;j<=cardinality[M];j++){
+					intermediateGain+=((float)classAttrValCount[i][j]/(float)attrValCount[i])*(log((float)classAttrValCount[i][j]/(float)attrValCount[i]));
+				}
+				intermediateGain*=(float(attrValCount[i])/(float)dataSize);
+				infoGain-=intermediateGain;
+			}
+			infoGains[bid]=infoGain;
+		}
+	}
+}
 
+__global__ void getInfoGainOfData(int *data,int dataSize,int *trainFileData,int *cardinality,int *infoGainOfData)
+{
+	__shared__ classValCount[cardinality[M]+1];
+	if(threadIdx.x==0){
+		for(i=0;i<=cardinality[M];i++){
+			classValCount[i]=0;
+		}
+	}
+	__syncthreads();
+	int classVal,i;
+	classVal=trainFileData(data[threadIdx.x],M);
+	atomicAdd(&classValCount[classVal],1);
+	__syncthreads();
+	float infoGain;
+	infoGain=0;
+	for(i=1;i<=cardinality[M];i++){
+		infoGain+=((double)classValCount[i]/(double)dataSize)*(log((double)classValCount/(double)dataSize));
+	}
+	infoGainOfData=-1*infoGain;
+}
 
 void decision(int *h_attr,int *h_data, node* root,int h_dataSize)
 {
-	int flag,h_selectedAttribute,i;
+	int flag,h_selectedAttribute,i,maxGain;
 	if(h_dataSize==0){
 		return;
 	}
@@ -111,15 +162,44 @@ void decision(int *h_attr,int *h_data, node* root,int h_dataSize)
 		return;
 	}
 
-	int *d_attr, *d_data, *d_selectedAttribute;
+	int *d_attr, *d_data;
+	float *d_infoGains,*d_infoGainOfData;
+	float h_infoGains[M-1], h_infoGainOfData;
 
 	cudaMalloc((void**)&d_attr,M*sizeof(int));
 	cudaMalloc((void**)&d_data,h_dataSize*sizeof(int));
-	cudaMalloc((void**)&d_selectedAttribute,sizeof(int));
+	cudaMalloc((void**)&d_infoGains,(M-1)*sizeof(float));
 	cudaMemcpy((void*)d_attr,(void*)h_attr,M*sizeof(int),cudaMemcpyHostToDevice);
 	cudaMemcpy((void*)d_data,(void*)d_data,N*sizeof(int),cudaMemcpyHostToDevice);
 
-	select<<<>>>
+	getInfoGains<<<blocks,h_dataSize>>>(d_attr,d_data,h_dataSize,d_infoGains,d_trainFileData,d_cardinality);
+
+	cudaMemcpy((void*)h_infoGains,(void*)d_infoGains,(M-1)*sizeof(float),cudaMemcpyDeviceToHost);
+
+	cudaMalloc((void**)&d_infoGainOfData,sizeof(float));
+
+	getInfoGainOfData<<<1,h_dataSize>>>(d_data,h_dataSize,d_trainFileData,d_cardinality,d_infoGainOfData);
+
+	cudaMemcpy((void*)h_infoGainOfData,(void*)d_infoGainOfData,sizeof(float),cudaMemcpyDeviceToHost);
+
+	maxGain=INT_MIN;
+	h_selectedAttribute=-1;
+	for(i=1;i<M-1;i++){
+		h_infoGains[i]=h_infoGainOfData-h_infoGains[i];
+		if(h_infoGains[i]>maxGain){
+			maxGain=h_infoGains[i];
+			h_selectedAttribute=i;
+		}
+	}
+
+	root->attribute = h_selectedAttribute;
+
+	if(h_selectedAttribute==-1){
+		root->val = popularVote(h_data, h_dataSize);
+		return;
+	}
+
+
 	
 }
 
@@ -176,7 +256,7 @@ int main()
 
 	cudaMalloc((void**)&d_cardinality,M*sizeof(int));
 	cudaMemset(d_cardinality,0,M*sizeof(int));
-	getCardinality<<<blocks,threads>>>(d_trainFileData,d_cardinality);
+	getCardinality<<<M,threads>>>(d_trainFileData,d_cardinality);
 
 	root = create();
 	decision(h_attr,h_data,root,N);
